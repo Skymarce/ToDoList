385b58465cb983858e640d018a0ab5ace953c278
Skymarce <81475820+Skymarce@users.noreply.github.com>
2021-12-10 11:08:49 +0300

    deleteTaskName

--- js/components/deleteTaskName.js
+++ js/components/deleteTaskName.js
@@ -1,43 +1,21 @@
 import {getData} from '../services/getData.js';
 import {deleteData} from '../services/deleteData.js';
-import {closeModal} from './modal.js';
 
 let deleteTask = document.querySelector('.delete-task');
+let taskInput = document.querySelector('.tasktitle-delete').value;
 
 function deleteTaskName(data) {
 
-    let taskInput = document.querySelector('.tasktitle-delete').value;
+    let filterCard = data.filter(elem => elem.taskTitle === taskInput);
 
-    
-
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].taskTitle === taskInput) {
-            deleteData(`http://localhost:3000/task/${data[i].id}`);
-        } /* else {
-            document.querySelector('.modal-window-delete').innerHTML += `
-                <div class="deletetaskname-error">Данная задача не найдена ;(</div>
-            `;
-        } */
-    }
-
-    /* data.forEach(item => {
-        if (taskInput === item.taskTitle) {
-            deleteData(`http://localhost:3000/task/${item.id}`);
-        } else {
-            document.querySelector('.modal-window-delete').innerHTML += `
-                <div class="deletetaskname-error">Данная задача не найдена ;(</div>
-            `;
-        }
-    }); */
+    filterCard.forEach(item => {
+        deleteData(`http://localhost:3000/task/${item.id}`);
+    });
 
 }
 
 deleteTask.addEventListener('click', () => {
     getData('http://localhost:3000/task').then(data => deleteTaskName(data));
 });
-
-/* document.querySelector('.close-modal-delete').addEventListener('click', () => {
-    closeModal('.modal-window-delete');
-}); */
 
 export {deleteTaskName};
